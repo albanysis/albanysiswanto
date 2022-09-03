6af161b872427aa09bfd30f9add14260aba9a637
Albanysis <albanysiswantoo@gmail.com>
2022-09-03 14:33:49 +0700

    first commit

---
+++ components/Hero.js
@@ -0,0 +1,33 @@
+import Image from 'next/image';
+import { hero } from '@/data/config';
+import { useTheme } from 'next-themes';
+
+export default function Hero() {
+  const { theme, setTheme } = useTheme();
+  return (
+    <div className="mb-20">
+      <div className="flex flex-row items-center justify-between w-full">
+        <div className="w-20 h-20 rounded-full relative overflow-hidden">
+          <Image
+            src="/static/pic.jpg"
+            layout="fill"
+            objectFit="contain"
+            alt="LeBron"
+          />
+        </div>
+        <Image
+          src="/static/icons/sun.svg"
+          width={30}
+          height={30}
+          alt="Toggle theme"
+          className="cursor-pointer toggleTheme"
+          onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+        />
+      </div>
+      <h1 className="mt-5 mb-4">{hero.title}</h1>
+      <p className="text-lg">{hero.desc}</p>
+      <br></br>
+      <a href={hero.link} className='border border-lightText rounded-lg px-2 py-1 hover:text-slate-900'>{hero.cv}</a>
+    </div>
+  );
+}
---
+++ data/config.js
@@ -0,0 +1,59 @@
+export const meta = {
+  title: 'Albany Siswanto',
+  description: 'Hey, I\'m Albany. I\'m a web developer and designer building things people love.'
+}
+
+export const hero = {
+  title: "Hallo Semua 👋, Saya Albany Siswanto.",
+  desc: "Saya seorang pengembang web dan perancang yang membangun hal-hal yang disukai orang.",
+  cv: "Download CV !",
+  link: 'https://www.cakeresume.com/pdf/s--bdYSCbO4MO8hbKllz2SKtA--/6rmvy.pdf'
+};
+
+export const projects = {
+  title: 'My Projects',
+  desc: "Berikut adalah beberapa proyek favorit saya yang telah saya kerjakan.",
+  projects: [
+    {
+      title: 'Start Foods Devlivered',
+      description: 'Online Foods Store - React & Tailwind',
+      link: 'https://start-eat.vercel.app/',
+      github: 'albanysis/start-eat.git'
+    },
+    {
+      title: 'Clone Netflix',
+      description: 'Netflix clone - React, Firebase & Tailwind',
+      link: 'https://clone-netfilx.vercel.app/',
+      github: 'albanysis/clone-netfilx.git'
+    },
+    {
+      title: 'Password Generator',
+      description: 'Password Generator - JavaScript',
+      link: 'https://password-generated-livid.vercel.app/',
+      github: 'albanysis/password-generated.git'
+    }
+  ]
+};
+
+export const stack = {
+  title: 'My Stack',
+  stack: [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'NextJS',
+    'Bootstrap',
+    'TailwindCSS',
+    'Firebase',
+    'Git'
+  ]
+};
+
+export const contact = {
+  title: 'Contact Me',
+  email: 'albanysiswantoo@gmail.com',
+  github: 'albanysis',
+  dribbble: 'albanysis',
+  linkedin: 'albany-siswanto-a48442248'
+};
